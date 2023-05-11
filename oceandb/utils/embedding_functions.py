1c6030154973c78fc17ff9c1d1a730bb33e85455
Kye <kye@apacmediasolutions.com>
2023-05-11 19:58:44 -0400

    convert tensors into numpy array then to a list

--- oceandb/utils/embedding_functions.py
+++ oceandb/utils/embedding_functions.py
@@ -63,8 +63,9 @@ class ImageBindEmbeddingFunction(EmbeddingFunction):
         with torch.no_grad():
             embeddings = self._model(inputs)
 
-        # Convert the embeddings ndarray to a list of embeddings
-        embeddings_list = [embedding for embedding in embeddings[self._modality]]
+        # Convert the embeddings tensor to a NumPy array and then to a list of embeddings
+        embeddings_array = embeddings[self._modality].cpu().numpy()
+        embeddings_list = [embedding for embedding in embeddings_array]
 
         return embeddings_list
 
